eec3a4767537ce607db7f517d30e712a2e8ad675
Adhithya Madesh <madeshauto64@gmail.com>
2024-06-06 14:29:29 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sample hosting
         </a>
       </header>
     </div>
